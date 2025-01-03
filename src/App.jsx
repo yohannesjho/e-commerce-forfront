45039e6b7a2f7dc8b3741552d11c1c5a3699965b
yohannesjho <jhonians@outlook.com>
2025-01-03 09:26:17 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Layout from './components/Layout'
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </Layout>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { BiUser } from 'react-icons/bi'
+import { BsSearch } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  const [ searchClicked, setSearchClicked ] = useState(false)
+
+  console.log(searchClicked)
+  return (
+    <header className='bg-gray-500 mx-auto text-white'>
+      <div className='flex justify-between items-center px-8 py-6 '>
+        <div>
+          <a href='/' className='text-2xl font-bold '>
+            E-commerce
+          </a>
+        </div>
+        <nav className='hidden md:flex space-x-4'>
+          <a href='/home'>Home</a>
+          <a href='/products'>Products</a>
+          <a href='/about'>About</a>
+          <a href='/contact'>Contacts</a>
+        </nav>
+        <div className='flex space-x-4 items-center'>
+          <input type='text' placeholder='search your product here' className={ searchClicked ? `px-4 py-1 w-64 rounded-md outline-none text-black` : `hidden` }/>
+          <BsSearch className='cursor-pointer' onClick={()=>setSearchClicked(!searchClicked)}/>
+          <BiUser />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
