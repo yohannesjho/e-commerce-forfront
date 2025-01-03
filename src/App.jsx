210dd9ebf8895f831043e864d527edca67a1db80
yohannesjho <jhonians@outlook.com>
2025-01-03 10:23:36 +0300

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import Home from './pages/Home'
 function App() {
   return (
     <>
