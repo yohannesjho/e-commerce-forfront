41789cc1428cb4e1d5e7bec5af9fdd52c44c12b7
yohannesjho <jhonians@outlook.com>
2025-01-03 10:32:59 +0300

    incorporate all routes to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+
 function App() {
   return (
     <>
@@ -8,6 +13,11 @@ function App() {
         <Layout>
           <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/products' element={<Products />}></Route>
+            <Route path='/product' element={<ProductDetails />}></Route>
+            <Route path='/about'  element={<About />}></Route>
+            <Route path="contact" element={<Contacts />}></Route>
+
           </Routes>
 
         </Layout>
