2a27c30c731a67c1be8641cdca13500a4256ada5
yohannesjho <jhonians@outlook.com>
2025-01-04 19:21:25 +0300

    Add remove item functionctionality from the cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { useCart } from '../contexts/cartContext'
 
 const Cart = () => {
-    const { cartItems } = useCart()
+    const { cartItems, removeFromCart} = useCart()
     console.log(cartItems)
+
+    const handleRemove = (id) => {
+        removeFromCart(id)
+    }
     return (
         <div className='grid gird-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-12 py-8 '>
             {cartItems.map((item) => (
                 <div key={item.id} className=''>
                     <div className='border w-32 h-32'>
 
-                        {item.img && item.img.length > 0 ? (<img src={item.img[0]} className='w-full h-full object-cover'/>) : (<p>Loading...</p>)}
+                        {item.img && item.img.length > 0 ? (<img src={item.img[0]} className='w-full h-full object-cover' />) : (<p>Loading...</p>)}
                     </div>
                     <div className='flex gap-4 '>
                         <span className='font-bold uppercase'>name:</span><p>{item.name}</p>
@@ -23,7 +27,7 @@ const Cart = () => {
                         <span className='font-bold uppercase'>price:</span><p>{item.quantity * item.price}</p>
                     </div>
                     <div className='mt-4'>
-                        <button className='bg-purple-600 px-2 py-1 rounded-md text-white  hover:bg-purple-200 duration-500'>Remove</button>
+                        <button onClick={() => { handleRemove(item.id) }} className='bg-purple-600 px-2 py-1 rounded-md text-white  hover:bg-purple-200 duration-500'>Remove</button>
                     </div>
 
                 </div>
