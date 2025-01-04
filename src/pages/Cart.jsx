8fb88163778dad44b4220efb88a6ca2f257e2fcb
yohannesjho <jhonians@outlook.com>
2025-01-04 19:40:09 +0300

    Add clear cart feature

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,39 +2,75 @@ import React from 'react'
 import { useCart } from '../contexts/cartContext'
 
 const Cart = () => {
-    const { cartItems, removeFromCart} = useCart()
+    const { cartItems, removeFromCart, clearCart } = useCart()
     console.log(cartItems)
 
     const handleRemove = (id) => {
         removeFromCart(id)
     }
+
+    const handleClearCart = () => {
+        clearCart()
+    }
     return (
-        <div className='grid gird-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-12 py-8 '>
-            {cartItems.map((item) => (
-                <div key={item.id} className=''>
-                    <div className='border w-32 h-32'>
+        <>
+            {cartItems && cartItems.length > 0 ? (
+                <div>
 
-                        {item.img && item.img.length > 0 ? (<img src={item.img[0]} className='w-full h-full object-cover' />) : (<p>Loading...</p>)}
-                    </div>
-                    <div className='flex gap-4 '>
-                        <span className='font-bold uppercase'>name:</span><p>{item.name}</p>
-                    </div>
 
-                    <div className='flex gap-4'>
-                        <span className='font-bold uppercase'>quantity:</span><p>{item.quantity}</p>
-                    </div>
-                    <div className='flex gap-4'>
-                        <span className='font-bold uppercase'>price:</span><p>{item.quantity * item.price}</p>
-                    </div>
-                    <div className='mt-4'>
-                        <button onClick={() => { handleRemove(item.id) }} className='bg-purple-600 px-2 py-1 rounded-md text-white  hover:bg-purple-200 duration-500'>Remove</button>
-                    </div>
+                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-12 py-8 border'>
+                        {cartItems.map((item) => (
+                            <div key={item.id} className=''>
+                                {/* Image */}
+                                <div className='border w-32 h-32'>
+                                    {item.img && item.img.length > 0 ? (
+                                        <img src={item.img[0]} className='w-full h-full object-cover' alt={item.name} />
+                                    ) : (
+                                        <p>Loading...</p>
+                                    )}
+                                </div>
+                                {/* Name */}
+                                <div className='flex gap-4'>
+                                    <span className='font-bold uppercase'>Name:</span>
+                                    <p>{item.name}</p>
+                                </div>
+                                {/* Quantity */}
+                                <div className='flex gap-4'>
+                                    <span className='font-bold uppercase'>Quantity:</span>
+                                    <p>{item.quantity}</p>
+                                </div>
+                                {/* Price */}
+                                <div className='flex gap-4'>
+                                    <span className='font-bold uppercase'>Price:</span>
+                                    <p>{item.quantity * item.price}</p>
+                                </div>
+                                {/* Remove Button */}
+                                <div className='mt-4'>
+                                    <button
+                                        onClick={() => handleRemove(item.id)}
+                                        className='bg-purple-600 px-2 py-1 rounded-md text-white hover:bg-purple-200 duration-500'
+                                    >
+                                        Remove
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
 
+
+                    </div>
+                    <div className='m-4 '>
+                        <button onClick={() => {handleClearCart()}} className='bg-red-500 px-2 py-1 rounded-md hover:bg-red-400 text-white duration-300'>clear cart</button>
+                    </div>
                 </div>
+            ) : (
+                <div className='text-center py-8'>
+                    <p>Your cart is empty!</p>
+                </div>
+            )}
+        </>
+    );
 
-            ))}
-        </div>
-    )
+
 }
 
 export default Cart
