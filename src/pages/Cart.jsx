ab1647a05a41c6d59dac6235744cbb892d0d367a
yohannesjho <jhonians@outlook.com>
2025-02-07 12:31:38 +0300

    make the cart responsive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
                 <div>
 
 
-                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 px-12 py-8 border'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 px-12 py-8 border'>
                         {cartItems.map((item) => (
                             <div key={item.id} className=' rounded-lg shadow-lg'>
                                 {/* Image */}
